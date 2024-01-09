d958e512bd244373a0542d741b93ead9003d103b
SergeyDubrovski <sovelectron@gmail.com>
2024-01-09 21:52:48 +0300

    1

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -21,7 +21,7 @@ export const MainPage = (props: Props) => {
       
       <textarea autoFocus className={s.decor}  value={line0.substring(0, 32)} 
       onChange={(e) => { setLine0(e.currentTarget.value)}}/>
-      <button className={s.buttonSend} onClick={()=>{sendMessage(line0.substring(0, 32));
+      <button className={s.buttonSend} onClick={()=>{sendMessage(line0.substring(0, 32)); setLine0('');
       }}>SEND</button>
     </div>
   )
